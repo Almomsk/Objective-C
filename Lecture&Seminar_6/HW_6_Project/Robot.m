44d66d482de15d00bcd9d12edcaaca58237391e2
Almomsk <almomsk@gmail.com>
2023-11-20 23:43:30 -0300

    HW_6 add

---
+++ Lecture&Seminar_6/HW_6_Project/Robot.m
@@ -0,0 +1,29 @@
+#import "Robot.h"
+
+@implementation Robot
+
+- (instancetype)initWithTileOrigin:(CGPoint)points andColor:(NSString *)robotName {
+    self = [super init];
+    if (self){
+        self.points = points;
+        self.robotName = robotName;
+    }
+    return self;
+}
+- (void)encodeWithCoder:(nonnull NSCoder *)coder {
+    [coder encodeObject:self.robotName forKey:@"name"];
+    [coder encodeFloat:self.points.x forKey:@"pointX"];
+    [coder encodeFloat:self.points.y forKey:@"pointY"];
+}
+
+- (nullable instancetype)initWithCoder:(nonnull NSCoder *)coder {
+    self = [super init];
+    if (self){
+        self.robotName = [coder decodeObjectForKey:@"name"];
+        float x = [coder decodeFloatForKey:@"pointX"];
+        float y = [coder decodeFloatForKey:@"pointY"];
+        self.points = CGPointMake(x, y);
+    }
+    return self;}
+
+@end
